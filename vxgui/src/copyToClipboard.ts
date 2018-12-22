173ddf551ff8eac0fa2ec57c70ecf42f0c79dad4
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-12-23 03:10:50 +0700

    Add a hack to allow web page to copy text

--- vxgui/src/copyToClipboard.ts
+++ vxgui/src/copyToClipboard.ts
@@ -1,10 +1,19 @@
 import { isElectronMode } from './environment'
 
 export function copyToClipboard(text: string) {
+  console.log('%cCopy: %s', 'font-weight:bold;font-size:24px;', text)
   if (isElectronMode) {
     const { clipboard } = (global as any).require('electron')
     clipboard.writeText(text)
   } else {
-    console.log('%cCopy: %s', 'font-weight:bold;font-size:24px;', text)
+    const copy = (window as any).copy
+    if (typeof copy === 'function') {
+      copy(text)
+    } else {
+      console.log(
+        'Pro-tip: Run this in JS console to enable clipboard integration:\n\n    ' +
+          "copy('...'); Object.assign(window, { copy })"
+      )
+    }
   }
 }
