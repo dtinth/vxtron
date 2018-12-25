a9b913a6f2a532724ce3e0cb3af77d2938af88fa
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-12-25 14:57:10 +0700

    Always load configuration from home directory

--- configuration.js
+++ configuration.js
@@ -1,5 +1,5 @@
 const cosmiconfig = require('cosmiconfig')
-const result = cosmiconfig('vx').searchSync()
+const result = cosmiconfig('vx').searchSync(process.env.HOME)
 if (!result) throw new Error('No ~/.vxrc.yml configuration found')
 const config = result.config
 module.exports = config
